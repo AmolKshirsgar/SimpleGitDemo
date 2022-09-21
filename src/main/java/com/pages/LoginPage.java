bc78570c7959062c3317b77ccd84660f3bb6297c
AmolKshirsagar <ap.kshirsagar01@gmail.com>
2022-09-21 17:51:38 +0530

    second commit

--- src/main/java/com/pages/LoginPage.java
+++ src/main/java/com/pages/LoginPage.java
@@ -4,7 +4,8 @@ public class LoginPage
 {
 	public void loginTest()
 	{
-		System.out.println("this is login test");
+		System.out.println("this is login Page");
+		System.out.println("this is login page new comment");  //updated and added line in eclipse
 	}
 
 }
